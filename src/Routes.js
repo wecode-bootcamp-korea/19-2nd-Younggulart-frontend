cc8c7eaefe23803a3b0b4d260b0242fd610de790
kimnamsun <nsunny0908@naver.com>
2021-05-06 11:23:07 +0900

    fix: clean npm cache fix: delete package-lock.json
    fix: lint warning

--- src/Routes.js
+++ src/Routes.js
@@ -2,28 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Main from './Pages/Main/Main';
-import Login from './Pages/Login/Login';
-import Loading from './Components/Loading/Loading';
-import Signup from './Pages/Signup/Signup';
 import Bids from './Pages/Bids/Bids';
-import Timer from './Pages/Main/Components/Timer';
 import Footer from './Components/Footer/Footer';
 
 const Routes = () => {
   return (
     <Router>
-      {/* <Nav /> */}
       <Switch>
-        {/* <Route exact path="/nav" component={Nav} /> */}
+        <Nav />
         <Route exact path="/bids" component={Bids} />
-        <Route exact path="/timer" component={Timer} />
-        {/* <Route exact path="/" component={Main} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/productList" component={ProductList} />
-        <Route exact path="/productList/:id" component={ProductDetail} />
-        <Route exact path="/cart" component={Cart} /> */}
+        <Route exact path="/" component={Main} />
+        {/* <Route exact path="/productList" component={ProductList} />
+        <Route exact path="/productList/:id" component={ProductDetail} /> */}
       </Switch>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 };
